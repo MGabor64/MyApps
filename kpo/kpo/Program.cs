0e9f0ba3c5a1c3da864f0fbf3706a0d775951758
MGabor64 <mgabor200311@gmail.com>
2019-03-06 12:08:59 +0100

    #2: játékos választásának bekérése

--- kpo/kpo/Program.cs
+++ kpo/kpo/Program.cs
@@ -10,16 +10,33 @@ namespace kpo
     {
         static void Main(string[] args)
         {   ///Program: Kő, papír, Olló játék
-            ///Írta: Maglódi Gábor, 2019.03.04
+            ///Írta: Maglódi Gábor, 2019.03.06
             ///Számítógép véletlenszerűen választ k/p/o közül
+            /// változók létrehozása
+            string compChoice="";
+            string myChoice="";
+            int compScore = 0, myScore = 0;
+
             bool moreGame = true;
             Console.WriteLine("===============================================Kő,Papír, Olló játék====================================================");
             do
             {
-                string compScore;
-                string mysSore;
-                int compscore = 0, myscore = 0;
+                ///játékos választásának bekérése és tárolása
+                Console.WriteLine("Mit választasz? k/p/o");
+                
+                switch(Console.ReadKey(true).KeyChar)
+                {
+                    case 'k':
+                        myChoice = "kő";
+                        break;
+                    case 'p':
+                        myChoice = "papír";
+                        break;
+                    case 'o':
+                        myChoice = "olló";
+                        break;
 
+                }
 
                 Console.Write("Akarsz még játszani? i/n");
                 if (Console.ReadKey(true).KeyChar == 'n')
