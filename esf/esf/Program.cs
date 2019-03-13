56c15d4d2d970c8759eebfa79839f65a9a3cfcea
MGabor64 <mgabor200311@gmail.com>
2019-03-13 12:04:49 +0100

    #4: ciklusok

--- esf/esf/Program.cs
+++ esf/esf/Program.cs
@@ -10,13 +10,31 @@ namespace esf
     {
         static void Main(string[] args)
         {
+            int v = 0;
             exercise ex = new exercise("Maglódi Gábor");
 
             Console.WriteLine(ex.Name);
             ex.PrintName();
 
             Console.WriteLine(ex.getName());
+            for(int i= 0; i< 2; i++)
+            {
+                Console.WriteLine("Ciklusváltozó hivaatkozás I.>0"+i);
+                Console.WriteLine("Ciklusváltozó hivaatkozás II.>0");
+            }
+            v = 2;
+            while (v>0)
+            {
+                Console.WriteLine("While ciklus");
+                v--;
+            }
+            do
+            {
+                Console.WriteLine("Do while ciklus");
+                v--;
+            } while (v>0);
             Console.ReadKey();
+
 
         }
     }
