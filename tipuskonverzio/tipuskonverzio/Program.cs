a3e963805528476713147d90ac7acb36052401ea
MGabor64 <mgabor200311@gmail.com>
2019-04-10 12:17:58 +0200

    #7: Tömbi

--- tipuskonverzio/tipuskonverzio/Program.cs
+++ tipuskonverzio/tipuskonverzio/Program.cs
@@ -8,6 +8,8 @@ namespace tipuskonverzio
 {
     class Program
     {
+        private static int iPrev;
+
         static void Main(string[] args)
         {
             int x = 10;
@@ -34,8 +36,41 @@ namespace tipuskonverzio
             {
                 Console.Write((int)ch);
             }
+            //tömbök
+            int[] iTömb = new int[10];
+            Console.WriteLine("");
+            Random r = new Random();
+            for (int i = 0; i < iTömb.Length; ++i)
+            {
+                //iTömb[i] = r.Next(0, 100);
+                Console.Write("Kérem a(z) " + i + ". számot: ");
+                try
+                {
+                    iTömb[i] = int.Parse(Console.ReadLine());
+                    iPrev = i;
+                }
+                catch ( Exception e)
+                {
+                    Console.WriteLine("Hiba! " + e.Message);
+                    i = iPrev;
+                }
+            }
 
+            
+            for(int i = 0; i < iTömb.Length; i++)
+            {
+                Console.Write(iTömb[i] + " ");
+            }
+            int[] iArr = new int[] { 1, 2, 3, 4 };
+            string[] sArr = new string[] { "alma", "körte","banán", "mangó" };
+            Array.Sort(sArr);
+            for (int i = 0; i < sArr.Length; i++)
+            {
+                Console.Write(sArr+" ");
 
+            }
+            //többdimenziós tömb
+            int[,] matrix = new int[3, 3];
             Console.ReadKey();
 
 
