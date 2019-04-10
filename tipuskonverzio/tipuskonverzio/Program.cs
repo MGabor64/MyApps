7d71c97896b5c04673db9f6b7b03fa3f6948d500
MGabor64 <mgabor200311@gmail.com>
2019-04-10 11:10:38 +0200

    #7: is as konverzió

---
+++ tipuskonverzio/tipuskonverzio/Program.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tipuskonverzio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            // az 'is' operátort fogjuk használni 
+            if(x is int)
+            {
+                Console.WriteLine("x típusa int");
+            }
+            //az 'as' operátort fogjuk használni
+            object a = "123";
+            object b = "HELLO";
+            object c = 10;
+            string aa = a as string;
+            Console.WriteLine(aa == null ? "NULL" : aa);//123
+
+            string bb = b as string;
+            Console.WriteLine(bb == null ? "NULL" : bb);//HELLO
+
+            string cc = c as string;
+            Console.WriteLine(cc == null ? "NULL" : cc);//NULL
+            Console.ReadKey();
+
+
+        }
+
+    }
+}
