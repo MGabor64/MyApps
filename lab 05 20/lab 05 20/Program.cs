60c094656ceccbdd15df9017fa7fdade090a846e
MGabor64 <mgabor200311@gmail.com>
2019-05-20 11:49:28 +0200

    #9:tömbs

--- lab 05 20/lab 05 20/Program.cs
+++ lab 05 20/lab 05 20/Program.cs
@@ -10,7 +10,36 @@ namespace lab_05_20
     {
         static void Main(string[] args)
         {
-            
+            //15 tagú tömb
+            int[] iArray = new int[15];
+            //5 tagú szöveg tömb
+            string[] sArray = new string[5];
+            //adattagos tömb
+            int[] iT = new int[] { 5, 8, 9 };
+            //2 dimenziós tömb
+            int[,] iT2 = new int[,] { { 4,5 },{ 2,3 },{ 6,8 } };
+            try
+            {
+                Console.WriteLine(iT2[2, 1]);
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+            //3 dimenziós tömb
+            int[,,] iT3 = new int[,,] { {{ 4, 5, 6 }, { 2, 3, 4 } } ,{ { 6, 8, 10 }, { 11, 12, 13 } } };
+            try
+            {
+                Console.WriteLine(iT3[1,1,0]);
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadLine();
         }
     }
 }
